23b163c6831a5c52692fbd10ea0e1c0afd8f69c8
M. Atif Siddiqui <56283783+atifsiddiqui10@users.noreply.github.com>
2022-02-26 14:41:14 -0600

    Automated

--- public/script/lineup.py
+++ public/script/lineup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Sun Feb  6 17:08:54 2022
 
@@ -12,18 +10,15 @@ import pandas as pd
 import sys
 from pandas.io.json import json_normalize
 import json
+import requests
 
-match_id = sys.argv[1]
+m_id = sys.argv[1]
 
-file_name=str(match_id)+'.json'
+to_fetch = m_id+'.json'
+l_site = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/lineups/"+to_fetch
+l_data = json.loads((requests.get(l_site)).text)
 
-
-with open('/Users/deepak/Documents/SoccerTact/data/lineups/'+file_name) as data_file:
-    #print (mypath+'events/'+file)
-    data = json.load(data_file)
-
-
-df = pd.json_normalize(data, sep = "_").assign(match_id = file_name[:-5])
+df = json_normalize(l_data, sep = "_").assign(match_id = m_id)
     
 away_team_lineup = df.loc[0, 'lineup']
 home_team_lineup = df.loc[1, 'lineup']
@@ -33,12 +28,13 @@ Awaynames = [nm['player_name'] for nm in away_team_lineup]
 
 
 json_string = json.dumps(Homenames, ensure_ascii=False)
-jsonFile = open("./public/analysis/home_line.json", "w")
+jsonFile = open("home_line.json", "w")
 jsonFile.write(json_string)
 jsonFile.close()
 
 
 json_string = json.dumps(Awaynames, ensure_ascii=False)
-jsonFile = open("./public/analysis/away_line.json", "w")
+jsonFile = open("away_line.json", "w")
 jsonFile.write(json_string)
 jsonFile.close()
+
