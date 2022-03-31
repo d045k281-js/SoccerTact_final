c201402a1562df83bb0db55b0e7edc48ec8a0fe2
M. Atif Siddiqui <56283783+atifsiddiqui10@users.noreply.github.com>
2022-03-31 12:53:04 -0500

    Get player_jersey number

---
+++ public/script/Number.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+import json
+from pandas import json_normalize
+
+def getNumber(m_id, l_data, name):
+  df = json_normalize(l_data, sep = "_").assign(match_id = m_id)    
+   
+    for x in df['lineup']:
+        for y in x:
+            if y['player_name'] == str(name):
+                return(y['jersey_number'])
